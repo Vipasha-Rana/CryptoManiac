82fa473929d165e00fb761913b1483a41f8e1afd
Vipasha-Rana <65186407+Vipasha-Rana@users.noreply.github.com>
2021-09-24 22:06:35 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Provider } from 'react-redux';
-
-import App from './App';
-import store from './app/store'
-
-import 'antd/dist/antd.css';
-
-ReactDOM.render(
-    <React.StrictMode>
-    <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
-  </React.StrictMode>,
-    document.getElementById('root')
-);
+export {default as Navbar} from './Navbar';
+export {default as Cryptocurrencies} from './Cryptocurrencies';
+export {default as CryptoDetails} from './CryptoDetails';
+export {default as Exchanges} from './Exchanges';
+export {default as Homepage} from './Homepage';
+export {default as News} from './News';
